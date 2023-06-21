f559918293226fac6ffae91ac50c1b46540d0bfe
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-06-21 15:43:18 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "b4bb3fa0-0129-4ae6-87f8-f2a88c6987c9",
+    "home_feed": [
+        {
+            "id": "e580bd25-769d-4fcb-8753-7da700136f3b",
+            "artist_name": "see",
+            "home_carousel": [
+                {
+                    "id": "478f5bf0-bcbc-42cb-b405-d624858898c9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "8052b91b-baa3-4444-b8a6-a8363c40c2aa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png"
+                },
+                {
+                    "id": "19687964-7059-4c39-af72-2094e9f738e5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                },
+                {
+                    "id": "d40ddcfb-9c1f-44ba-b40c-7756c4b5fc41",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png"
+                },
+                {
+                    "id": "437ed848-c7de-464a-a8b7-da6f9fc77216",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+                }
+            ],
+            "art_caption": "down",
+            "art_description": "High improve letter room particularly. Concern none nor move want."
+        },
+        {
+            "id": "b1d08335-ee85-4f5b-9b79-6fad34366768",
+            "artist_name": "head",
+            "home_carousel": [
+                {
+                    "id": "4ec13310-33bd-4794-93b6-576f7bf12b8c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+                },
+                {
+                    "id": "e4e5de8a-6af5-4ab4-b350-cc8db83505cd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/329.png"
+                },
+                {
+                    "id": "4a3a0027-4af5-41be-bba8-eb667917bf38",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                },
+                {
+                    "id": "de2d4f62-486c-4b19-84bb-2e37192cc926",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/51.png"
+                },
+                {
+                    "id": "332d1bf2-700e-4802-85b9-9df25e51aa58",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                }
+            ],
+            "art_caption": "be",
+            "art_description": "Remember hospital foreign. Study article sound decide sometimes TV."
+        },
+        {
+            "id": "31c18f44-df9a-4947-980c-736a4659121a",
+            "artist_name": "majority",
+            "home_carousel": [
+                {
+                    "id": "1c7f334a-fcbb-41fb-b967-82c9a924ba6d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/120.png"
+                },
+                {
+                    "id": "c503c535-a138-405d-b933-f45bd3e19702",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                },
+                {
+                    "id": "52c551b1-ca2b-41ef-93af-2deba2cd6251",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+                },
+                {
+                    "id": "96e7eb55-0643-4228-ac70-dd7d9ffd17a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/75.png"
+                },
+                {
+                    "id": "53139f25-56ff-4347-8e09-831736cfc78b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png"
+                }
+            ],
+            "art_caption": "figure",
+            "art_description": "Drop state company report let. Early mean or keep site. Sell today cold position become tonight matter."
+        },
+        {
+            "id": "794b2bf7-0bce-4b1d-8044-5ec9fb102605",
+            "artist_name": "expert",
+            "home_carousel": [
+                {
+                    "id": "7045ee3b-4287-4e53-8523-3cb7868e7d8e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                },
+                {
+                    "id": "890d605b-743d-4fb7-a059-7d8fcc01b8bc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/193.png"
+                },
+                {
+                    "id": "8d540d5d-e060-40e4-b2e7-22d1f7e72d9d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/60.png"
+                },
+                {
+                    "id": "e4c481aa-8f96-4dc7-b7ea-2e521b0ea39e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "1be884e3-b74a-4190-a139-f5539182b6e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/296.png"
+                }
+            ],
+            "art_caption": "finally",
+            "art_description": "Follow term ever authority. Production before occur sing technology small give them. Past us too simple."
+        },
+        {
+            "id": "1e470252-0495-42c2-8ac0-20512d3d7ab3",
+            "artist_name": "same",
+            "home_carousel": [
+                {
+                    "id": "7095cdfc-b319-423d-b22d-afe1806f20cb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/120.png"
+                },
+                {
+                    "id": "2bed2e8d-0610-45b3-b9b8-0bb8c0e7b678",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/50.png"
+                },
+                {
+                    "id": "edf27324-25ae-4e2a-9690-ae11a08ed666",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "3dac04ee-bc85-4d12-8392-6fda6b2b8232",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/170.png"
+                },
+                {
+                    "id": "c48c004f-fa76-414e-964f-41f79cddcc2a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/280.png"
+                }
+            ],
+            "art_caption": "thousand",
+            "art_description": "May reveal ago mention theory. Only and land."
+        }
+    ]
+}
